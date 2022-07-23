fa41501bb2ff7d4ac6d33f49a8c6d3627a3f798f
Rohit <stylishrb711@gmail.com>
2022-07-24 00:06:38 +0530

    form created

--- client/src/Components/Forms/Form.jsx
+++ client/src/Components/Forms/Form.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./form.css";
+import FileBase from "react-file-base64";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
 
-export default Form
+  function changeHandler(e) {
+    const { name, value } = e.target;
+    setPostData({
+      ...postData,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
+  return (
+    <div className="form-div">
+      <div className="form-title">Create a post</div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group w-75 container">
+          <input
+            name="creator"
+            type="text"
+            className="form-control my-4"
+            placeholder="Creator"
+            value={postData.creator}
+            onChange={changeHandler}
+          />
+
+          <input
+            name="title"
+            type="text"
+            className="form-control my-4"
+            placeholder="Title"
+            value={postData.title}
+            onChange={changeHandler}
+          />
+
+          <textarea
+            name="message"
+            class="form-control my-4"
+            rows="3"
+            placeholder="Enter message"
+            value={postData.message}
+            onChange={changeHandler}
+          ></textarea>
+
+          <input
+            name="tags"
+            type="text"
+            className="form-control"
+            placeholder="Tags"
+            value={postData.tags}
+            onChange={changeHandler}
+          />
+          <div className="file-input-div my-2">
+            <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) =>
+                setPostData({ ...postData, selectedFile: base64 })
+              }
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 my-2">
+            Submit
+          </button>
+          <button onClick={clear} className="btn btn-danger w-100">
+            Clear
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
