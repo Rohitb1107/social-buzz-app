fea567208171c698a4b220aae8894df31ef8a6f8
Rohit <stylishrb711@gmail.com>
2022-07-25 14:16:25 +0530

    css update

--- client/src/Components/Posts/Post/Post.jsx
+++ client/src/Components/Posts/Post/Post.jsx
@@ -3,24 +3,27 @@ import moment from "moment";
 import { useDispatch } from "react-redux";
 import { deletePost, likePost } from "../../../redux/actions/posts";
 import "./post.css";
+import editlogo from "../../../assests/edit.svg";
+import deletelogo from "../../../assests/delete.svg";
+import likelogo from "../../../assests/like.svg";
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   return (
     <div className="post-card-div">
-      <div className="row img-part">
+      <div className="row img-div">
         <img className="post-img" src={post.selectedFile} alt="" />
       </div>
       <div className="row other-part">
         <div className="row">
-          <div className="col-6">tags</div>
-          <div className="col-6">
-            <button
-              className="btn btn-outline-info"
+          <div className="col-6 tags-part">#love #actor</div>
+          <div className="col-6 edit-part">
+            <img
+              className="editlogo"
               onClick={() => setCurrentId(post._id)}
-            >
-              Edit
-            </button>
+              src={editlogo}
+              alt=""
+            />
           </div>
         </div>
         <div className="row creator-name">{post.creator}</div>
@@ -29,20 +32,22 @@ const Post = ({ post, setCurrentId }) => {
         <div className="row msg-part">{post.message}</div>
         <div className="row like-del-part">
           <div className="col-6">
+            <edit />
             <button
               className="btn btn-outline-primary"
               onClick={() => dispatch(likePost(post._id))}
             >
+              <img className="editlogo" src={likelogo} alt="" />
               LIKE: {post.likeCount}
             </button>
           </div>
-          <div className="col-6">
-            <button
-              className="btn btn-outline-danger"
+          <div className="col-6 delete-part">
+            <img
+              className="editlogo"
               onClick={() => dispatch(deletePost(post._id))}
-            >
-              DELETE
-            </button>
+              src={deletelogo}
+              alt=""
+            />
           </div>
         </div>
       </div>
